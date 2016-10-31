fca615a49c96317f6465d7222cf6a4f2d98c8132
Nate Walck <nate.walck@gmail.com>
2016-10-31 16:47:19 -0700

    Updated to work with Ubuntu

--- zsh/antibody.zsh
+++ zsh/antibody.zsh
@@ -1,5 +1,23 @@
 # Path to antigen
 
-source <(antibody init)
+# auto pull in antibody
+apphome=${0:a:h}
 
-antibody bundle < "$MYZSH/antibody-bundles.txt"
+[[ ! -d $apphome/bundle ]] && mkdir $apphome/bundle
+[[ ! -d $apphome/bundle/antibody ]] && git clone https://github.com/yanyingwang/antibody.git $apphome/bundle/antibody
+#[[ ! -d $apphome/bundle/oh-my-zsh ]] && git clone https://github.com/robbyrussell/oh-my-zsh.git $apphome/bundle/oh-my-zsh 
+
+source $apphome/bundle/antibody/antibody.zsh
+
+#antibody bundle < "$MYZSH/antibody-bundles.txt"
+
+
+antibody github zsh-users/zsh-completions
+antibody github zsh-users/zsh-history-substring-search
+antibody github bhilburn/powerlevel9k
+
+if [ $(uname) = "Linux" ]; then
+  source  "$apphome/bundle/github-bhilburn-powerlevel9k/powerlevel9k.zsh-theme"
+else
+  antibody github zsh-users/zsh-syntax-highlighting
+fi
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,2 +1,6 @@
-export PATH=/Library/Frameworks/Python.framework/Versions/3.5/bin:$PATH
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+if [ $(uname) = "Linux" ]; then
+else
+  export PATH=/Library/Frameworks/Python.framework/Versions/3.5/bin:$PATH
+  export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+fi
+
