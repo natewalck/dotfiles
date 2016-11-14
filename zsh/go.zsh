f91e5f20cc3eb61a04f547751dc338acb461779b
Nate  Walck <nate.walck@gmail.com>
2016-11-13 19:55:22 -0800

    Added conditional go bin path for linux

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,2 +1,5 @@
- export GOPATH=$HOME/code/go
- export PATH=$PATH:$GOPATH/bin
+if [ $(uname) = "Linux" ]; then
+    export PATH=$PATH:/usr/local/go/bin
+fi
+export GOPATH=$HOME/code/go
+export PATH=$PATH:$GOPATH/bin
