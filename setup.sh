525ce79efc322c25b26f07589004ba9b36e029e0
Nate Walck <nate.walck@gmail.com>
2020-07-15 09:44:46 -0400

    More tweaks to setup

--- setup.sh
+++ setup.sh
@@ -10,10 +10,6 @@ fi
 # Disable the accent menu when holidng a button down
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Symlink subklime text to subl for easy access
-sudo mkdir -p /usr/local/bin
-sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-
 # Command to show battery percent
 defaults write com.apple.menuextra.battery ShowPercent YES
 
