3c85efba6c2ad4ca8f3746604984bc3a4e7552ff
Nathan Walck <github@natewalck.com>
2021-07-12 10:26:57 -0400

    Add gcloud paths

---
+++ zsh/gcloud.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/gcloud/path.zsh.inc' ]; then . '/opt/gcloud/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/opt/gcloud/completion.zsh.inc' ]; then . '/opt/gcloud/completion.zsh.inc'; fi
