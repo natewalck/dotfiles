0afe45a85801e146593d57cc177df54bbffd8047
Nathan Walck <github@natewalck.com>
2021-10-09 14:17:01 -0400

    Check for brew to exist before running it

--- zsh/brew.zsh
+++ zsh/brew.zsh
@@ -1,5 +1,5 @@
-if [ $(arch) = "i386" ]; then
+if [[ $(arch) = "i386" && -f "/usr/local/bin/brew" ]]; then
     eval $(/usr/local/bin/brew shellenv)
-else
+elif [ -f "/opt/homebrew/bin/brew" ]; then
     eval $(/opt/homebrew/bin/brew shellenv)
 fi
