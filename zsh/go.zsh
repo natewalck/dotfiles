bf61a41af965efe36a2e4173c5d231ffdd8e3712
Nate Walck <github@natewalck.com>
2025-07-03 16:49:39 -0400

    Add newer changes to config

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,3 +1,4 @@
+# export PATH=${PATH}:`go env GOPATH`/bin
 export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/gocode
-export PATH=$PATH:$GOPATH/bin
+# export GOPATH=$HOME/go
+export PATH=$PATH:$HOME/go/bin
